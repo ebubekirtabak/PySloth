8d7ef03572555cde4923658f9b5f9ec6e0fec4a5
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-04 21:43:47 +0300

    event maker added.

---
+++ event_maker.py
@@ -0,0 +1,66 @@
+import time
+
+from selenium.common.exceptions import NoSuchElementException
+
+
+class EventMaker:
+
+    driver = None
+    def __init__(self, driver):
+        self.driver = driver
+        pass
+
+    def push_event(self, driver, event):
+        if driver is not None:
+
+            try:
+                element = driver.find_element_by_xpath(event['selector'])
+                if element is not None:
+                    if 'delay' in event:
+                        time.sleep(event['delay'])
+                    for action in event['actions']:
+                        if 'delay' in action:
+                            time.sleep(action['delay'])
+
+                        switcher = {
+                            "click": self.set_click,
+                            "scroll": self.set_scroll,
+                            "style": self.set_style,
+                            "excute_script": self.set_excute_script,
+                            "nothing": lambda: self.nothing,
+                        }
+
+                        func = switcher.get(action['type'], lambda: "nothing")
+                        func(element, action)
+
+                    if 'sleep' in event:
+                        time.sleep(event['sleep'])
+
+            except NoSuchElementException as e:
+                print("-- NoSuchElementException: " + str(e))
+
+            except  Exception as e:
+                print("start_thread_error: " + str(e))
+
+        else:
+            return None
+
+
+    def set_click(self, element, action):
+        element.click()
+
+    def set_scroll(self):
+        return ""
+
+    def set_style(self, element, action):
+        scriptSetAttrValue = "arguments[0].setAttribute(arguments[1],arguments[2])"
+        self.driver.execute_script(scriptSetAttrValue, element, 'style', action['style'])
+
+    def set_excute_script(self, element, action):
+        self.driver.execute_script(action['script'])
+
+
+    def nothing(self, element, action):
+        return ""
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Konfigürasyon Ayarları
+#### settings
+##### $thread_limit
+'Progress Thread limit.'
