9f95768274e35154b5249f055ca8e7f096de8e08
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-08 23:01:18 +0300

    mongo thread controll developments

--- controllers/mongo_thread_controller.py
+++ controllers/mongo_thread_controller.py
@@ -1,5 +1,8 @@
 import logger
 import mongo
+import time
+import json
+from bson import json_util
 
 class MongoThreadController:
     active_thread_array = []
@@ -16,13 +19,28 @@ class MongoThreadController:
 
     def thread_controller(self):
         print("mongo thread_controller")
+        try:
+            logger.set_log("Mongo Thread Controller : Active : " +
+                       str(len(self.active_thread_array)) + " : Array : " +
+                       str(len(self.thread_array)))
+            lent = mongo.get_length(self.database, self.database_setting['thread_collection_name'])
+            if len(self.active_thread_array) < self.settings["thread_limit"] and lent > 0:
+                print("bhjbjh")
+
+        except Exception as e:
+            logger.set_error_log("auto_thread_stopper(): " + str(e))
+            time.sleep(10)
+            self.thread_controller()
 
     def add_thread(self, thread_model):
-        logger.set_log("added Theread : " + thread_model.name)
+        logger.set_log("added Thread : " + thread_model.name)
         if len(self.active_thread_array) < self.settings['thread_limit']:
-            self.thread_array.append(thread_model)
+            self.active_thread_array.append(thread_model)
         else:
-            mongo.insert(self.database, self.database_setting['thread_collection'], thread_model)
+            dt = {}
+            dt.update(vars(thread_model))
+            json_model = json.dumps(dt)
+            mongo.insert(self.database, self.database_setting['thread_collection_name'], json_model)
 
         self.thread_controller()
 
@@ -31,5 +49,26 @@ class MongoThreadController:
 
     def clear_thread_list(self):
         self.empty_thread_step = 0
-        self.thread_array = []
         self.active_thread_array = []
+
+    def auto_thread_stopper(self):
+        # then timeout stop thread
+
+        try:
+            index = 0
+            for thread in self.active_thread_array:
+                now_time = int(round(time.time() * 1000))
+                different = (now_time - thread.start_time)
+                different = int(different / 1000)
+                if different > self.settings['thread_time_out']:
+                    print(thread.name + ' Timeout...')
+                    thread.stop_time = now_time
+                    self.add_thread(self.active_thread_array[index])
+                    self.active_thread_array.remove(self.active_thread_array[index])
+
+            index += 1
+
+        except Exception as e:
+            logger.set_error_log("auto_thread_stopper(): " + str(e))
+            time.sleep(10)
+            self.thread_controller()
