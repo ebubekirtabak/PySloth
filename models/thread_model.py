f17e438f63edbd0acd0adc6e18d12e0131517b5c
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-15 23:23:31 +0300

    added start_time property.

--- models/thread_model.py
+++ models/thread_model.py
@@ -2,12 +2,27 @@
 class ThreadModel:
 
     def __init__(
-            self, name=None, target=None, args=None,
+            self,name=None, target=None, args=None,
             status=None, type=None, start_time=0, stop_time=0):
         self.name = name
         self.target = target
         self.args = args
         self.status = status
         self.type = type
-        self.start_time = start_time
+        self._start_time = start_time
         self.stop_time = stop_time
+
+    def to_dict(self):
+        return [("name", self.name), ("target", self.target), ("args", self.args),
+                ("status", self.status), ("type", self.type), ("start_time", self.start_time),
+                ("stop_time", self.stop_time)]
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+
+    @start_time.setter
+    def start_time(self, start_time):
+        self._start_time = start_time
+
