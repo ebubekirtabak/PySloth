643f358729bac19cf0cc3dd633b367ea63a0682d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-20 21:29:12 +0300

    added write_file_line method

--- modules/file_module.py
+++ modules/file_module.py
@@ -1,11 +1,20 @@
 import time
 import logger
+import os
+
+script_dir = os.path.dirname(__file__)
 
 
 class FileModule:
 
     def __init__(self, file_name):
         self.file_name = file_name
+
+
+    @staticmethod
+    def check_directory(dir):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 
     @staticmethod
     def get_short_file_name(file_name, max_len):
@@ -27,3 +36,14 @@ class FileModule:
             logger.set_error_log("File name could not be shortened. (" + file_name + "): "
                                  + str(e))
             return file_name
+
+    @staticmethod
+    def write_file_line(self, dir=script_dir, file_name=None, line=None):
+        try:
+            self.check_directory(dir)
+            abs_file_path = os.path.join(dir, file_name)
+            with open(abs_file_path, 'a') as the_file:
+                the_file.write(line)
+                the_file.write('\n')
+        except Exception as e:
+            print(e)
