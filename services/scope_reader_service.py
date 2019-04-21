9edb1e936fda5d570d79a6a37e028d40137c2a41
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-21 13:18:21 +0300

    added scope reader service

--- services/scope_reader_service.py
+++ services/scope_reader_service.py
@@ -1,4 +1,22 @@
+import json
+
+from modules.file_module import FileModule
+
+
 class ScopeReaderService:
 
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.init_scope_model(name)
+
+    def init_scope_model(self, scope_name):
+        try:
+            read_file = FileModule.read_json_file(file_name='myconf.json')
+            if read_file['success'] is True:
+                data = read_file['data']
+                print(data)
+                scope = data['scope']
+                for item in scope:
+                    if item.scope_name == scope_name:
+                        print(item)
+        except Exception as e:
+            print(str(e))
