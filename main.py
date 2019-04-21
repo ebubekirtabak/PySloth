999a125982b3ef1e18be7f641ba5eede61d49dd8
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-21 17:41:43 +0300

    scope reader connect to main

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 import scope
 import sys
 
+from services.scope_reader_service import ScopeReaderService
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == 'scope':
         scope.init_scope()
+    scopeModel = ScopeReaderService().init_scope_model('Dollar')
+    print('scope_model')
+
 
 
 if __name__ == '__main__':
