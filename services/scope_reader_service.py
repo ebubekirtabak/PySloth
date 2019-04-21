1db8a9e6839170d2d728e02588287d28f2f50d9e
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-21 23:14:15 +0300

    Auto stash before merge of "master" and "origin/master"

--- services/scope_reader_service.py
+++ services/scope_reader_service.py
@@ -1,6 +1,8 @@
 import json
+from collections import namedtuple
 
 from modules.file_module import FileModule
+from models.setting_model import SettingModel
 
 
 class ScopeReaderService:
@@ -8,8 +10,15 @@ class ScopeReaderService:
     def __init__(self):
         pass
 
-    @staticmethod
-    def init_scope_model(scope_name):
+    def get_scope(self, scope_name):
+        scope = self.init_scope_model(scope_name)
+        settins_dict = scope["settings"]
+        driver_dict = scope["driver"]
+        setting = namedtuple("SettingModel", settins_dict.keys())(*settins_dict.values())
+        driver = namedtuple("DriverModel", driver_dict.keys())(*driver_dict.values())
+        print(setting.session_id)
+
+    def init_scope_model(self, scope_name):
         try:
             read_file = FileModule.read_json_file(file_name='myconf.json')
             if read_file['success'] is True:
