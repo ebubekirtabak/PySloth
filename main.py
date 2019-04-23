fee252c5d2995dc123d89c94cd23ed18bb70691f
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-23 18:31:15 +0300

    space fix

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 
 from services.scope_reader_service import ScopeReaderService
 from scope import Scope
+
+
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == 'scope':
         scope_model = ScopeReaderService().get_scope(scope_name=sys.argv[2])
