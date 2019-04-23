05209189aa3707f447bba4818207d339b733e60b
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-23 14:24:25 +0300

    logger fix

--- logger.py
+++ logger.py
@@ -1,24 +1,24 @@
 import time
 import os
+
+from modules.file_module import FileModule
 script_dir = os.path.dirname(__file__)
 
 
-def set_log(data):
-    abs_file_path = os.path.join(script_dir, 'log.txt')
-    with open(abs_file_path, 'a') as the_file:
-        the_file.write( str(time.strftime('%c')) + " : " + data)
-        the_file.write('\n')
+class Logger:
 
+    def __init__(self):
+        pass
 
-def set_error_log(data):
-    abs_file_path = os.path.join(script_dir, 'error_log.txt')
-    with open(abs_file_path, 'a') as the_file:
-        the_file.write( str(time.strftime('%c')) + " : " + data)
-        the_file.write('\n')
+    def set_log(self, data):
+        abs_file_path = os.path.join(script_dir, 'log.txt')
+        with open(abs_file_path, 'a') as the_file:
+            the_file.write( str(time.strftime('%c')) + " : " + data)
+            the_file.write('\n')
 
-def set_memory_log(data):
-    abs_file_path = os.path.join(
-        script_dir, 'memory_logs/error_log' + str(time.time()) + '.txt')
-    with open(abs_file_path, 'a') as the_file:
-        the_file.write( str(time.strftime('%c')) + " : " + data)
-        the_file.write('\n')
+    def set_error_log(self, data):
+        print("Error:" + data)
+        FileModule.write_file_line(script_dir, 'error_log.txt', 'Error: ' + str(time.strftime('%c')) + " : " + data)
+
+    def set_memory_log(self, data):
+        FileModule.write_file_line(script_dir, 'memory_logs/error_log' + str(time.time()) + '.txt',  str(time.strftime('%c')) + " : " + data)
