61a11f70ec5f7b90edd7a36a3860f4976dedb9e5
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-23 14:23:00 +0300

    init main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import scope
 import sys
 
 from services.scope_reader_service import ScopeReaderService
-
+from scope import Scope
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == 'scope':
-        scope.init_scope()
-    scopeModel = ScopeReaderService().init_scope_model('Dollar')
-    print('scope_model')
+        scope_model = ScopeReaderService().get_scope(scope_name=sys.argv[2])
+        Scope(scope_model).start()
 
 
 
