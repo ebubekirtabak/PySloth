b98107c39772ffcc9f382dda349d72ca502b4274
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-23 14:23:40 +0300

    setting model changed

--- models/setting_model.py
+++ models/setting_model.py
@@ -1,3 +1,7 @@
 class SettingModel:
-    def __init__(self, driver):
+    def __init__(self, driver, database, multi_process, history, role):
         self.driver = driver
+        self.database = database
+        self.multi_process = multi_process
+        self.history = history
+        self.role = role
