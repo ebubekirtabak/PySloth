05bd78179588869a1aa91666749a59942574f1da
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-23 14:24:37 +0300

    changed history py

--- history.py
+++ history.py
@@ -1,27 +1,37 @@
 import mongo
 
-''' "history": {
-    "to": "database",
-    "collection_name": "history_collection",
-    "control": {
-        "from": "database",
-        "progress": "skip"
-    }
-    } '''
+''' 
+    "history": {
+        "to": "database",
+        "collection_name": "history_collection",
+        "control": {
+            "from": "database",
+            "progress": "skip"
+        }
+    } 
+    '''
+
+class history_control:
+
+    def __init__(self, history):
+        self.history = history
 
 
+    def history_control(self, history, data):
+        control = self.history["control"]
+        switcher = {
+            "database": self.database_control(history, data),
+            "file": self.file_control(history),
+        }
+
+        func = switcher.get(control["from"], lambda: "nothing")
+        return func
+
+    def database_control(self, control, data):
+        exit()
 
 
-def history_control(history, data):
-    control = history["control"]
-    switcher = {
-        "database": database_control(history, data),
-        "file": file_control(history),
-    }
+    def file_control(self, control, data):
+        exit()
 
-    func = switcher.get(contro["from"], lambda: "nothing")
-    return func
-
-def database_control(control, data):
-    exit()
 
