1eb231ba8f5f46870288dc764ada098fe54af4d8
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-23 14:24:46 +0300

    added url helpers

---
+++ helpers/url_helpers.py
@@ -0,0 +1,25 @@
+import urllib
+import urllib.request
+from logger import Logger
+from lxml.html import fromstring
+
+
+class UrlHelpers:
+    def __init__(self):
+        pass
+
+    def get_page_html_content(self, url, data=None, headers={}):
+        try:
+            req = urllib.request.Request(
+                url,
+                data=data,
+                headers=headers
+            )
+            f = urllib.request.urlopen(req)
+            content = f.read()
+            doc = fromstring(content)
+            doc.make_links_absolute(url)
+        except ConnectionResetError as e:
+            Logger().set_error_log(str(e))
+        except Exception as e:
+            Logger().set_error_log(str(e))
