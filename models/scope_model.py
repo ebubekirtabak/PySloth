76d8d5a460981bd09e05a0ea33a6fc6ea8b70452
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-23 14:23:09 +0300

    added scope model

---
+++ models/scope_model.py
@@ -0,0 +1,8 @@
+class ScopeModel:
+
+    def __init__(self, settings, search_item, page, reporting):
+        self.settings = settings
+        self.search_item = search_item
+        self.page = page
+        self.reporting = reporting
+
