43502e6d67592be077fc3c70646f0ba34bc319c3
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-24 21:20:12 +0300

    added file settings

--- models/setting_model.py
+++ models/setting_model.py
@@ -1,7 +1,8 @@
 class SettingModel:
-    def __init__(self, driver, database, multi_process, history, role):
+    def __init__(self, driver, database, multi_process, history, role, file_settings):
         self.driver = driver
         self.database = database
         self.multi_process = multi_process
         self.history = history
         self.role = role
+        self.file_settings = file_settings
