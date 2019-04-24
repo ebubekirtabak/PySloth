d0e88657d0204a22f04e49a95c47d4dc34d1d6b4
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-25 00:02:16 +0300

    bug fix

--- logger.py
+++ logger.py
@@ -18,7 +18,7 @@ class Logger:
 
     def set_error_log(self, data):
         print("Error:" + data)
-        FileModule.write_file_line(script_dir, 'error_log.txt', 'Error: ' + str(time.strftime('%c')) + " : " + data)
+        FileModule().write_file_line(script_dir, file_name='error_log.txt', line='Error: ' + str(time.strftime('%c')) + " : " + data)
 
     def set_memory_log(self, data):
-        FileModule.write_file_line(script_dir, 'memory_logs/error_log' + str(time.time()) + '.txt',  str(time.strftime('%c')) + " : " + data)
+        FileModule().write_file_line(script_dir, file_name='memory_logs/error_log' + str(time.time()) + '.txt',  line=str(time.strftime('%c')) + " : " + data)
