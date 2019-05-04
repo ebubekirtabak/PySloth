0e22daf5104442642b3c0a705c97447f03e8431d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-05-04 22:08:29 +0300

    driver model updated.

--- models/driver_model.py
+++ models/driver_model.py
@@ -1,4 +1,6 @@
 class DriverModel:
-    def __init__(self, driver_path, driver_arguments):
+    def __init__(self, driver_path, driver_arguments, driver_emulations, driver_extensions):
         self.driver_path = driver_path
         self.driver_arguments = driver_arguments
+        self.driver_emulations = driver_emulations
+        self.driver_extensions = driver_extensions
