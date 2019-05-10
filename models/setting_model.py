03c5fc4c35d343753b83140cb14a0769e19c126c
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-05-11 01:38:06 +0300

    is_go_again_history property added.

--- models/setting_model.py
+++ models/setting_model.py
@@ -1,8 +1,11 @@
 class SettingModel:
-    def __init__(self, driver, database, multi_process, history, role, file_settings):
+    def __init__(self, driver, database, multi_process, history, role, file_settings,
+                 is_go_again_history):
         self.driver = driver
         self.database = database
         self.multi_process = multi_process
         self.history = history
         self.role = role
         self.file_settings = file_settings
+        self.is_go_again_history = is_go_again_history
+
