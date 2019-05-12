5b5104744d1301c2b7586a7171c702fc78e7e760
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-05-12 16:44:56 +0300

    added login in scope model

--- models/scope_model.py
+++ models/scope_model.py
@@ -1,8 +1,11 @@
+from models.login_model import LoginModel
+
+
 class ScopeModel:
 
-    def __init__(self, settings, search_item, page, reporting):
+    def __init__(self, settings, search_item, page, reporting, login: LoginModel):
         self.settings = settings
         self.search_item = search_item
         self.page = page
         self.reporting = reporting
-
+        self.login = login
