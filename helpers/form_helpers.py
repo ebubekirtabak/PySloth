4cb8c117b860bc214e374cb82d30723dec8dec57
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-05-13 22:45:12 +0300

    added NoSuchElementException to form helpers

--- helpers/form_helpers.py
+++ helpers/form_helpers.py
@@ -1,5 +1,8 @@
+import sys
 from collections import namedtuple
 
+from selenium.common.exceptions import NoSuchElementException
+from logger import Logger
 
 class FormHelpers:
 
@@ -8,11 +11,20 @@ class FormHelpers:
         pass
 
     def submit_form(self, form):
-        form = namedtuple("FormModel", form.keys())(*form.values())
-        for input in form.inputs:
-            input = namedtuple("InputModel", input.keys())(*input.values())
-            input_element = self.driver.find_element_by_xpath(input.selector)
-            input_element.send_keys(input.value)
+        try:
+            form = namedtuple("FormModel", form.keys())(*form.values())
+            for input in form.inputs:
+                input = namedtuple("InputModel", input.keys())(*input.values())
+                input_element = self.driver.find_element_by_xpath(input.selector)
+                input_element.send_keys(input.value)
 
-        submit = self.driver.find_element_by_xpath(form.submit['selector'])
-        submit.click()
+            submit = self.driver.find_element_by_xpath(form.submit['selector'])
+            submit.click()
+        except NoSuchElementException as e:
+            type, value, traceback = sys.exc_info()
+            if hasattr(value, 'filename'):
+                print('Error %s: %s' % (value.filename, value.strerror))
+                Logger().set_error_log('Error %s: %s' % (value.filename, value.strerror))
+
+            Logger().set_error_log('submit_form() -> ' + str(e))
+
