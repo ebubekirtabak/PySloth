cf2dcc3d00b1a24f9e62fc1905abc07250b561d7
Ebubekir <etabak@borusan.com>
2019-05-14 12:41:40 +0300

    added script action model.

---
+++ models/script_action_model.py
@@ -0,0 +1,7 @@
+class ScriptActionModel:
+    def __init__(self, type, event, value, selector, desc ):
+        self.type = type
+        self.event = event
+        self.value = value
+        self.selector = selector
+        self.desc = desc
