7a58b3d455a4203bd596041da70c8cc1b805a530
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-07-18 21:45:00 +0300

    added scope file property.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from collections import namedtuple
+
 import scope
 import sys
 
+from modules.file_module import FileModule
 from services.scope_reader_service import ScopeReaderService
 from scope import Scope
 
@@ -9,7 +12,10 @@ def main():
     if len(sys.argv) > 1 and sys.argv[1] == 'scope':
         scope_model = ScopeReaderService().get_scope(scope_name=sys.argv[2])
         Scope(scope_model).start()
-
+    elif len(sys.argv) > 1 and sys.argv[1] == 'scope_file':
+        file = FileModule().read_file(dir=sys.argv[2], file_name=sys.argv[3])
+        scope_model = namedtuple("ScopeModel", file['data'].keys())(*file['data'].values())
+        Scope(scope_model).start()
 
 
 if __name__ == '__main__':
