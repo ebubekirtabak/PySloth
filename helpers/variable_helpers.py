ffd99353a85caa72ae11b8adcf7f45e9e9153844
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-07-28 22:16:14 +0300

    added VariableHelpers.

--- helpers/variable_helpers.py
+++ helpers/variable_helpers.py
@@ -4,3 +4,6 @@ class VariableHelpers:
     global scope_variables = {};
     def __init__(self):
         pass
+
+    def set_variable(self, variable_name, variable_value):
+        scope_variables[variable_name] = variable_value
