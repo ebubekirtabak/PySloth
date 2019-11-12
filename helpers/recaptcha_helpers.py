2f462eaf2bb9af3d3598bfaf21470f6b7e42fa6e
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-11-12 19:14:05 +0300

    Solve captcha func created.

--- helpers/recaptcha_helpers.py
+++ helpers/recaptcha_helpers.py
@@ -1,10 +1,150 @@
-class RecaptchaHelpers:
+from urllib.request import urlopen
 
+import urllib3
+from PIL import Image
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+import requests
+from io import BytesIO
+
+from helpers.image_helpers import ImageHelpers
+
+
+class RecaptchaHelpers:
+    object_maps = {
+        'bicycles': [
+            'bicycles',
+            'bike',
+            'cycles',
+            'cycle'
+        ],
+        'vehicles': [
+            'vehicles',
+            'vehicle',
+            'car',
+            'cars'
+        ],
+        'cars': [
+            'vehicles',
+            'vehicle',
+            'car',
+            'cars'
+        ],
+        'car': [
+            'vehicles',
+            'vehicle',
+            'car',
+            'cars'
+        ],
+        'palm trees': [
+            'palm trees',
+            'palm tree',
+            'tree',
+            'tropical'
+        ],
+        'traffic lights': [
+            'light',
+            'traffic lights',
+            'traffic light'
+        ],
+        'crosswalks': [
+            'crosswalks',
+            'crossing'
+        ]
+    }
     def __init__(self):
         pass
 
     def __load_image(self):
         pass
 
-    def solveCaptcha(self, image_url, keyword, size):
-        pass
+    def solve_captcha(self, image_url, keyword, size, api_key, image_size):
+        image_result = []
+        cropped_images = []
+        response = requests.get(image_url)
+        img_data = response.content
+        image = Image.open(BytesIO(img_data))
+        size = self.get_calculate_size(image.size, image_size, size)
+        w_level = int(image.size[0] / size['width'])
+        h_level = int(image.size[0] / size['height'])
+        index = 0
+        h = 0
+        for h in range(h_level):
+            for i in range(w_level):
+                left = (i * size['width'])
+                top = (h * size['height'])
+                right = (i * size['width']) + size['width']
+                bottom = (h * size['height']) + size['height']
+                border = (left, top, right, bottom)
+                cropped_image = ImageHelpers().crop_image(image, border)
+                image_name = str(left) + "_" + str(top) + "_" + str(right) + "_" + str(bottom) + ".jpeg"
+                cropped_images.append(image_name)
+                cropped_image.save('./' + image_name)
+                result = self.get_objects_from_image('./' + image_name, api_key)
+                print(border)
+                if result['code'] != 400:
+                    is_exists = self.find_object_in_concepts(keyword,result['concepts'])
+                    image_result.append({'w': i, 'h': h, 'is_exists': is_exists})
+                else:
+                    image_result.append({'w': i, 'h': h, 'is_exists': False})
+        print(image_result)
+        return image_result
+
+    def get_objects_from_image(self, image, api_key):
+        app = ClarifaiApp(api_key=api_key)
+        model = app.public_models.general_model
+        model.model_version = 'aa7f35c01e0642fda5cf400f543e7c40'
+        image = ClImage(filename=image)
+        response = model.predict([image])
+        if response['status']['code'] == 10000:
+            return {'code': 10000, 'concepts': self.parse_response_data(response['outputs'])}
+        else:
+            return {'code': 400}
+
+    def parse_response_data(self, outputs):
+        output = outputs[0]
+        data = output['data']
+        concepts = data['concepts']
+        return concepts
+
+    def get_calculate_size(self, image, image_size, size):
+        image_width = image[0]
+        image_height = image[1]
+        if image_width > image_size['width']:
+            diff = image_width - image_size['width']
+            width_rate =int((diff / image_size['width']) * 100)
+            size['width'] = int(size['width'] + ((size['width'] / 100) * width_rate))
+        else:
+            diff = image_size['width'] - image_width
+            width_rate = int((diff / image_width) * 100)
+            size['width'] = int(size['width'] - ((size['width'] / 100) * width_rate))
+
+        if image_height > image_size['height']:
+            diff = image_height - image_size['height']
+            height_rate = int((diff / image_size['height']) * 100)
+            size['height'] = int(size['height'] + ((size['height'] / 100) * height_rate))
+        else:
+            diff = image_size['height'] - image_height
+            height_rate = int((diff / image_height) * 100)
+            size['height'] = int(size['height'] - ((size['height'] / 100) * height_rate))
+
+        return size
+
+    def find_object_in_concepts(self, keyword, concepts):
+        if keyword in self.object_maps.keys():
+            keyword_list = self.object_maps[keyword]
+            for item in concepts:
+                try:
+                    index = keyword_list.index(item['name'])
+                    print('****' + keyword + ' == ' + item['name'])
+                    return True
+                except:
+                    print(keyword + ' -> ' + item['name'])
+        else:
+            for item in concepts:
+                print(keyword + ' -> ' + item['name'])
+                if keyword == item['name']:
+                    return True
+
+        return False
+
