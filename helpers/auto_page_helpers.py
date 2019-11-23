92950d0485fc81df5877bcf2889cbf6490edd915
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-11-23 14:41:22 +0300

    created solve_recaptcha func.

--- helpers/auto_page_helpers.py
+++ helpers/auto_page_helpers.py
@@ -1,3 +1,5 @@
+import time
+
 import globals
 
 
@@ -9,4 +11,23 @@ class AutoPageHelpers:
     def check_page_elements(self):
 
         if len(self.doc.find_elements_by_xpath(globals.recaptcha_xpath)) > 0:
-            print("ere")
+            self.solve_recaptcha()
+
+    def solve_recaptcha(self):
+        frame = self.doc.find_element_by_xpath("//div[@class='g-recaptcha']/div/div/iframe")
+        self.doc.switch_to.frame(frame)
+
+        anchor = self.doc.find_element_by_xpath("//*[@id='recaptcha-anchor']")
+        anchor.click()
+        time.sleep(2)
+        self.doc.switch_to.default_content()
+
+        frame = self.doc.find_element_by_xpath("//*[@title='recaptcha challenge']")
+        self.doc.switch_to.frame(frame)
+        time.sleep(2)
+        audio_button = self.doc.find_element_by_xpath("//button[@id='recaptcha-audio-button']")
+        audio_button.click()
+
+        self.doc.switch_to.default_content()
+        self.doc.switch_to.frame(frame)
+
