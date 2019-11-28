2a1915b19b38eb4430abbdbb5b13c27de055fbf6
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-11-28 23:13:49 +0300

    Get style from element func added into ElementHelpers.

--- helpers/element_helpers.py
+++ helpers/element_helpers.py
@@ -3,12 +3,18 @@ class ElementHelpers:
     def __init__(self):
         pass
 
-    @staticmethod
-    def get_attribute_from_element(element, attr):
-        if attr == 'text':
+    def get_attribute_from_element(self, element, attr):
+        if attr.startswith('style:'):
+            return self.get_style_from_element(element, attr.split(':')[1])
+        elif attr == 'text':
             return element.text
         elif attr == 'size':
             return element.size
         else:
             return element.get_attribute(attr)
+
+    @staticmethod
+    def get_style_from_element(element, attr):
+        return  element.value_of_css_property(attr)
+
 
