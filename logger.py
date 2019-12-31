07051979c3b6e3caaf3e1c0565c1e613bdb43663
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-12-31 17:34:41 +0300

    set error log fix.

--- logger.py
+++ logger.py
@@ -1,8 +1,12 @@
+import sys
 import time
 import os
+import globals
 
+
+from modules.file_module import FileModule
 script_dir = os.path.dirname(__file__)
-
+logs_dir = script_dir + '/logs/'
 
 class Logger:
 
@@ -18,12 +22,25 @@ class Logger:
             the_file.write( str(time.strftime('%c')) + " : " + data)
             the_file.write('\n')
 
-    def set_error_log(self, data, is_print_to_console=False):
+    @staticmethod
+    def set_error_log(data, is_print_to_console=False):
         if is_print_to_console:
             print("Error:" + data)
 
-        from modules.file_module import FileModule
-        FileModule().write_file_line(script_dir, file_name='error_log.txt', line='Error: ' + str(time.strftime('%c')) + " : " + data)
+        session_id = globals.configs['session_id']
+        FileModule().write_file_line(
+            logs_dir,
+            file_name='log_' + session_id + '.txt',
+            line=time.strftime('%c') + " : " + str(data)
+        )
+        value, traceback = sys.exc_info()
+        if hasattr(value, 'filename'):
+            print('Error %s: %s' % (value.filename, value.strerror))
+            FileModule().write_file_line(
+                logs_dir,
+                file_name='log_' + session_id + '.txt',
+                line=time.strftime('%c') + " : " + 'Error %s: %s' % (value.filename, value.strerror)
+            )
 
     def set_memory_log(self, data):
         from modules.file_module import FileModule
