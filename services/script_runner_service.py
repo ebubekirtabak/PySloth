a760e62c53fe7dc3e81dfbcc8cbbd073df02f41a
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-01-03 14:56:28 +0300

    run script func created.

--- services/script_runner_service.py
+++ services/script_runner_service.py
@@ -1,5 +1,19 @@
+import subprocess
+
+from logger import Logger
+
+
 class ScriptRunnerService:
 
     def __init__(self, script_options):
         self.script_options = script_options
         self.logger = Logger()
+
+    def run(self):
+
+        type = self.script_options['type']
+
+        if type == "python":
+            subprocess.call(['python3', self.script_options['script']])
+        elif type == "python3":
+            subprocess.call(['python3', self.script_options['script']])
