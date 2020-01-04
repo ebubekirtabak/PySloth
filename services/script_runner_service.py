873bc9779dc12e08644c14e0fb68d0efc35cffaa
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-01-04 13:38:20 +0300

    get script result fix.

--- services/script_runner_service.py
+++ services/script_runner_service.py
@@ -18,14 +18,24 @@ class ScriptRunnerService:
         elif type == "python3":
             subprocess.call(['python3', self.script_options['script']])
 
-    def get_script_result(self):
+    def get_script_result(self, *params):
+        try:
+            type = self.script_options['type']
+            process_output = ''
+            if type == "python":
+                process_output = subprocess.check_output('python ' + self.script_options['script'] + ' ' + params, shell=True)
+            elif type == "python3":
+                command = ['python3', self.script_options['script']]
+                for param in params:
+                    command.append(param)
 
-        type = self.script_options['type']
-        process_output = ''
-        if type == "python":
-            process_output = subprocess.check_output(['python3', self.script_options['script']])
-        elif type == "python3":
-            process_output = subprocess.check_output(['python3', self.script_options['script']])
+                self.logger.set_log("run script: " + str(command))
+                process_output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
 
-        self.logger.set_log("get_script_result: " + process_output)
-        return process_output
+            result = process_output.decode("utf-8")
+            self.logger.set_log("script result: " + result, True)
+            return result
+        except Exception as e:
+            self.logger.set_log("custom_script Exception: " + str(e), True)
+            return ''
+
