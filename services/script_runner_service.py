7381c8733cfed390ba9f74a8aee7b4e437cbbe35
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-01-06 14:25:30 +0300

    script_runner_service array params issue fix.

--- services/script_runner_service.py
+++ services/script_runner_service.py
@@ -18,22 +18,32 @@ class ScriptRunnerService:
         elif type == "python3":
             subprocess.call(['python3', self.script_options['script']])
 
-    def get_script_result(self, *params):
+    def get_script_result(self, params):
         try:
             type = self.script_options['type']
             process_output = ''
             if type == "python":
                 process_output = subprocess.check_output('python ' + self.script_options['script'] + ' ' + params, shell=True)
             elif type == "python3":
-                command = ['python3', self.script_options['script']]
-                for param in params:
-                    command.append(param)
+                if isinstance(params, list):
+                    command = ['python3', self.script_options['script']]
+                    result = []
+                    for param in params:
+                        command.append(param)
+                        self.logger.set_log("run script: " + str(command))
+                        process_output = subprocess.check_output(command, shell=False, stderr=subprocess.PIPE)
+                        result.append(process_output.decode("utf-8"))
 
-                self.logger.set_log("run script: " + str(command))
-                process_output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+                else:
+                    command = ['python3', self.script_options['script']]
+                    command.append(params)
 
-            result = process_output.decode("utf-8")
-            self.logger.set_log("script result: " + result, True)
+                    self.logger.set_log("run script: " + str(command))
+                    process_output = subprocess.check_output(command, shell=False, stderr=subprocess.PIPE)
+                    result = process_output.decode("utf-8")
+                    self.logger.set_log("script result: " + result, True)
+
+
             return result
         except Exception as e:
             self.logger.set_log("custom_script Exception: " + str(e), True)
