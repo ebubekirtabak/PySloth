c65c03fa891bff3d315920cbf6bc44f1a27cfe95
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-01-06 20:28:10 +0300

    time_out feature added SettingsModel

--- models/setting_model.py
+++ models/setting_model.py
@@ -1,6 +1,6 @@
 class SettingModel:
     def __init__(self, driver, database, multi_process, history, role, file_settings,
-                 is_go_again_history, clarifia_api_key, is_page_helper):
+                 is_go_again_history, clarifia_api_key, is_page_helper, time_out):
         self.driver = driver
         self.database = database
         self.multi_process = multi_process
@@ -10,4 +10,5 @@ class SettingModel:
         self.is_go_again_history = is_go_again_history
         self.is_page_helper = is_page_helper
         self.clarifia_api_key = clarifia_api_key
+        self.time_out = time_out
 
