8cae1e5c9d7b55eb7bbbd662d738b422fe8fd44d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2020-10-16 17:29:38 +0300

    Add get request method.

--- helpers/http_helpers.py
+++ helpers/http_helpers.py
@@ -9,6 +9,23 @@ class HttpHelpers:
         self.logger = Logger()
 
     def send_request(self, request_data):
+        method = request_data["method"]
+        if method == "post":
+            self.post_request(request_data)
+        if method == "get":
+            self.get_request_content(request_data)
+
+    def get_request_content(self, request_data):
+        try:
+            url = request_data["url"]
+            self.logger.set_log("Http Request: " + url, True)
+            resp = requests.get(url, params=request_data["params"], headers=request_data["headers"])
+            self.logger.set_log("Http Response: " + str(resp), True)
+            return resp
+        except Exception as e:
+            self.logger.set_log('send_request(): ' + str(e), True)
+
+    def post_request(self, request_data):
         try:
             url = request_data["url"]
             self.logger.set_log("Http Request: " + url, True)
