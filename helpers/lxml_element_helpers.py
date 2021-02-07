e513643bc45f4db8a40f0c3d41856e98164a4296
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-02-07 22:41:56 +0300

    Create get_src_from_element function in LXMLElementHelpers.

--- helpers/lxml_element_helpers.py
+++ helpers/lxml_element_helpers.py
@@ -11,8 +11,24 @@ class LXMLElementHelpers:
             return element.text
         elif attr == 'size':
             return element.size
+        elif attr == 'src':
+            return self.get_src_from_element(element, action_object)
         else:
-            return element.attribute[attr]
+            return element.attrib[attr]
+
+    @staticmethod
+    def encode_image(image_url, action_object):
+        convert_type = action_object["as"]
+        if convert_type == "base64":
+            return ImageHelpers().encode_base64_from_url(image_url)
+
+    def get_src_from_element(self, element, action_object):
+        attr = action_object['attribute_name']
+        src = element.get_attribute(attr)
+        if "as" in action_object:
+            return self.encode_image(src, action_object)
+        else:
+            return src
 
     @staticmethod
     def get_style_from_element(element, attr):
